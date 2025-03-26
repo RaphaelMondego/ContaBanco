24293ddc36066cf147258a747380cb3ff8a9a806
raphael.mondego <raphamondego@gmail.com>
2025-03-26 17:30:05 -0300

    todo's

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,8 @@
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        //TODO: Conhecer e importar a classe Scanner
+        //Exibir as mensagens para o nosso usuário
+        //Obter pelo scanner os valores digitados no terminal
+        //Exibir a mensagem da conta criada
     }
 }
